d2c1ca0d20168c6c5c91c396a5e3e93afee839fe
Kaaveh Mohamedi <Kaaveh@pm.me>
2024-03-08 19:20:19 +0330

    Add starter code

---
+++ source-code/starter-project/app/src/main/java/com/droidcon/tinyinvoice/domain/model_provider/PreviewBusinessProvider.kt
@@ -0,0 +1,34 @@
+package com.droidcon.tinyinvoice.domain.model_provider
+
+import com.droidcon.tinyinvoice.domain.model.Business
+
+val businessList = listOf(
+    Business(
+        id = 0,
+        name = "DroidCon",
+        address = "DeutschLand",
+        phone = "+981938",
+        email = "example@gmail.com",
+    ),
+    Business(
+        id = 1,
+        name = "DroidCon",
+        address = "DeutschLand",
+        phone = "+981938",
+        email = "example@gmail.com",
+    ),
+    Business(
+        id = 2,
+        name = "DroidCon",
+        address = "DeutschLand",
+        phone = "+981938",
+        email = "example@gmail.com",
+    ),
+    Business(
+        id = 3,
+        name = "DroidCon",
+        address = "DeutschLand",
+        phone = "+981938",
+        email = "example@gmail.com",
+    ),
+)
